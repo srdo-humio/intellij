samkenxstream/turnkey-triumph-326606_samkencoin-intellij

Use sync strategy to determine blaze info binary path
Adds a utility method to BuildSystem for returning a binary path. If the sync strategy is PARALLEL, the method returns the path from a parallel invoker, if it exists. In all other cases, the default path is returned.

This ensures that initial invocations of `info` and `query` use the parallel invoker binary when applicable. When sync strategy is DECIDE_AUTOMATICALLY, the local invoker is used through the query invocation.

PiperOrigin-RevId: 451024320

base/src/com/google/idea/blaze/base/sync/ProjectStateSyncTask.java
@@ -106,12 +106,15 @@ final class ProjectStateSyncTask {
             BlazeCommandName.INFO,
             context,
             BlazeInvocationContext.SYNC_CONTEXT);
+
     ListenableFuture<BlazeInfo> blazeInfoFuture =
         BlazeInfoRunner.getInstance()
             .runBlazeInfo(
                 context,
                 importSettings.getBuildSystem(),
-                Blaze.getBuildSystemProvider(project).getBinaryPath(project),
+                Blaze.getBuildSystemProvider(project)
+                    .getBuildSystem()
+                    .getDefaultBinaryPath(project, context),
                 workspaceRoot,
                 syncFlags);
 

srdo-humio/intellij

base/src/com/google/idea/blaze/base/sync/ProjectStateSyncTask.java
@@ -104,12 +104,15 @@ final class ProjectStateSyncTask {
             BlazeCommandName.INFO,
             context,
             BlazeInvocationContext.SYNC_CONTEXT);
+
     ListenableFuture<BlazeInfo> blazeInfoFuture =
         BlazeInfoRunner.getInstance()
             .runBlazeInfo(
                 context,
                 importSettings.getBuildSystem(),
-                Blaze.getBuildSystemProvider(project).getBinaryPath(project),
+                Blaze.getBuildSystemProvider(project)
+                    .getBuildSystem()
+                    .getDefaultSyncBinaryPath(project, context),
                 workspaceRoot,
                 syncFlags);
 